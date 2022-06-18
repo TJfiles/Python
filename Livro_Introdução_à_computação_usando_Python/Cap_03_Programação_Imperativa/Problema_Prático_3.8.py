0d88aa5d7ff5a930c4c86356a1b50f46011f2693
Johnny <63070273+TJfiles@users.noreply.github.com>
2022-06-18 04:57:44 -0300

    Problema Prático 3.8

---
+++ Livro_Introdução_à_computação_usando_Python/Cap_03_Programação_Imperativa/Problema_Prático_3.8.py
@@ -0,0 +1,13 @@
+"""
+Defina, diretamente no shell interativo, a função média(), que aceita dois números como
+entrada e retorna a média dos números. Um exemplo de uso é:
+>>> average(2, 3.5)
+2.75
+"""
+
+
+def media(a, b):
+    return (a+b)/2
+
+
+print(media(2, 3.5))
