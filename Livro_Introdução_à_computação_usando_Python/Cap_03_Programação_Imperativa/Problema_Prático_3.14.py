e047c0f92c3e567df8a62937c381d21579b32e6b
Johnny <63070273+TJfiles@users.noreply.github.com>
2022-06-18 05:28:01 -0300

    Problema Prático 3.14

---
+++ Livro_Introdução_à_computação_usando_Python/Cap_03_Programação_Imperativa/Problema_Prático_3.14.py
@@ -0,0 +1,22 @@
+"""
+Implemente a função trocaPU(), que aceita uma lista como entrada e troca o primeiro e último
+elementos da lista. Você pode considerar que a lista não estará vazia. A função não deverá retornar
+nada.
+>>> ingredientes = ['farinha', 'açúcar', 'manteiga', 'maçãs']
+>>> trocaPU(ingredientes)
+>>> ingredientes
+['maçãs', 'açúcar', 'manteiga', 'farinha']
+"""
+
+ingredients = ['farinha', 'açúcar', 'manteiga', 'maçãs']
+
+
+def trocaPU(lst):
+    last = lst.pop()
+    first = lst.pop(0)
+    lst.append(first)
+    lst.insert(0, last)
+
+
+trocaPU(ingredients)
+print(ingredients)
