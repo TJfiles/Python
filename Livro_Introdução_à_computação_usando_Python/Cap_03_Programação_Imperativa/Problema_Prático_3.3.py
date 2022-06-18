be2c9cc4d4ac08dc8188aa838441ab1bddb22e33
Johnny <63070273+TJfiles@users.noreply.github.com>
2022-06-18 04:29:46 -0300

    Problema Prático 3.3

---
+++ Livro_Introdução_à_computação_usando_Python/Cap_03_Programação_Imperativa/Problema_Prático_3.3.py
@@ -0,0 +1,24 @@
+"""
+Traduza estas declarações em instruções if/else do Python:
+(a)Se ano é divisível por 4, exiba 'Pode ser um ano bissexto.'; caso contrário,
+exiba 'Definitivamente não é um ano bissexto.'
+(b)Se a lista bilhete é igual à lista loteria, exiba 'Você ganhou!'; se não,
+exiba 'Melhor sorte da próxima vez…'.
+"""
+# (a)Se ano é divisível por 4, exiba 'Pode ser um ano bissexto.'; caso contrário,
+# exiba 'Definitivamente não é um ano bissexto.'
+'''
+if (year % 4 ==0):
+    print('Pode ser um ano bissexto.')
+else:
+    print('Definitivamente não é um ano bissexto.')
+'''
+
+# (b)Se a lista bilhete é igual à lista loteria, exiba 'Você ganhou!'; se não,
+# exiba 'Melhor sorte da próxima vez…'.
+'''
+if lista_bilhete == lista_loteria:
+    print('Você ganhou!')
+else:
+    print('Melhor sorte da próxima vez...')
+'''
