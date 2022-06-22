40aad08aa545843fa1e33ec72a400f92fcf23746
Johnny <63070273+TJfiles@users.noreply.github.com>
2022-06-22 19:14:17 -0300

    Problema_Prático 5.3

---
+++ Livro_Introdução_à_computação_usando_Python/Cap_05_Estruturas_de_Controle_de_Execução/Problema_Prático_5.3.py
@@ -0,0 +1,38 @@
+"""
+Escreva a função aritmética(), que aceita uma lista de inteiros como entrada e
+retorna True se eles formarem uma sequência aritmética. (Uma sequência de inteiros é
+uma sequência aritmética se a diferença entre os itens consecutivos da lista for sempre a mesma.)
+>> aritmética([3, 6, 9, 12, 15])
+True
+>> aritmética([3, 6, 9, 11, 14])
+False
+>> aritmética([3])
+True
+"""
+
+
+'''def aritmetica(lst):
+    """Retorna True se a lista lst tiver uma sequência aritmética, False se caso contrário"""
+    if len(lst) > 2:'''
+
+
+
+
+'''    dif_base = 0
+    print(lst)
+    for i in range(0, len(lst)-1):
+        dif = lst[i+1] - lst[i]
+        print('A diferença entre {} e {} é {}'.format(lst[i], lst[i+1], dif))
+        if i == 0:
+            dif_base = dif
+        elif dif_base != dif:
+            return False
+
+        elif dif_base == dif:
+            return True
+        '''
+
+print(aritmetica([3, 6, 9, 12, 15]))
+
+print()
+print(aritmetica([3, 6, 9, 11, 14]))
